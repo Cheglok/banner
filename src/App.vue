2e9763ce88b08a35cd1c71a3091a02694e061df0
Cheglok <cheglok37@gmail.com>
2025-02-22 12:40:33 +0500

    feat: start banner project second iteration

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,28 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue';
-import TheWelcome from './components/TheWelcome.vue';
-</script>
-
 <template>
-    <header>
-        <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-        <div class="wrapper">
-            <HelloWorld msg="You did it!" />
-        </div>
-    </header>
-
-    <main>
-        <TheWelcome />
-    </main>
+    <div class="frame">
+        <TopPart class="frame__top" />
+        <BottomPart class="frame__bottom" />
+    </div>
 </template>
 
+<script setup lang="ts">
+import TopPart from '@/components/TopPart.vue';
+import BottomPart from '@/components/BottomPart.vue';
+</script>
+
 <style scoped>
-header {
-    line-height: 1.5;
+.frame {
+    display: flex;
+    flex-direction: column;
+    width: 100vw;
+    height: 100vh;
+    background: black;
+    overflow: hidden;
 }
-
-.logo {
-    display: block;
-    margin: 0 auto 2rem;
+.frame__top {
+    flex-grow: 4;
 }
-
-@media (min-width: 1024px) {
-    header {
-        display: flex;
-        place-items: center;
-        padding-right: calc(var(--section-gap) / 2);
-    }
-
-    .logo {
-        margin: 0 2rem 0 0;
-    }
-
-    header .wrapper {
-        display: flex;
-        place-items: flex-start;
-        flex-wrap: wrap;
-    }
+.frame__bottom {
+    flex-grow: 6;
 }
 </style>
