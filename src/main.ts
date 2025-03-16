6cb979d92020dacc3e1d4f9105bab01762d58cff
Cheglok <cheglok37@gmail.com>
2025-03-16 21:51:59 +0500

    refactor: убрал лишние файлы

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,7 @@ import './assets/main.css';
 
 import { createApp } from 'vue';
 import App from './App.vue';
-import { registerIconsGlobal } from '@/components/Icons/registerIconsGlobal.ts';
 
 const app = createApp(App);
-
-registerIconsGlobal(app);
 
 app.mount('#app');
